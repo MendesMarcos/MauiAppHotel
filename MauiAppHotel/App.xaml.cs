44a8461dbcc538f7e083fbd938960953bcc2d5b2
Marcos Mendes <marcos.v.mendes1@gmail.com>
2024-12-16 15:06:55 -0300

    Add project files.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiAppHotel
+{
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+
+         //MainPage = new AppShell();
+      }
+
+      protected override Window CreateWindow(IActivationState? activationState)
+      {
+         try
+         {
+            var window = new Window(new AppShell());
+            //var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+         }
+         catch (Exception ex)
+         {
+            throw new Exception(ex.Message);
+         }
+      }
+   }
+}
